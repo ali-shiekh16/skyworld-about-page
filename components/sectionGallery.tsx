06795b4a23693cf734774be8bea357f1b29bacda
Mohammad Ali <ali.shiekh16@outlook.com>
2023-01-25 21:40:59 +0500

    Remove unecessary code.

--- components/sectionGallery.tsx
+++ components/sectionGallery.tsx
@@ -1,14 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import useWindowSize from '../hooks/windowSize';
+import React from 'react';
 import styles from '../styles/About.module.scss';
 import Button from './button';
 
 function SectionGallery() {
-  const breakpoint = 768;
-
-  const size = useWindowSize();
-  // console.log(size);
-
   const renderGalleryContent = () => {
     return (
       <>
@@ -31,60 +25,6 @@ function SectionGallery() {
             <Button text1='See how we help' text2='communities' />
           </div>
         </div>
-
-        {size.width < breakpoint && (
-          <div className={styles.section_gallery__text_img_container}>
-            {/* <img src='/8.png' className={styles.section_gallery__text_img} /> */}
-          </div>
-        )}
-      </>
-    );
-  };
-
-  const renderImages = () => {
-    console.log(size.width < breakpoint);
-    console.log(size.width);
-
-    if (size.width < breakpoint)
-      return (
-        <>
-          {/* {renderGalleryContent()}
-          <div className={styles.section_gallery__images}>
-            <img src='/1.png' className={styles.section_gallery__img_1} />
-            <img src='/2.png' className={styles.section_gallery__img_2} />
-            <img src='/3.png' className={styles.section_gallery__img_3} />
-            <img src='/4.png' className={styles.section_gallery__img_4} />
-            <img src='/5.png' className={styles.section_gallery__img_5} />
-            <img
-              src='/hands.png'
-              className={styles.section_gallery__img_hands}
-            />
-            <img src='/7.png' className={styles.section_gallery__img_7} />
-            <img src='/8.png' className={styles.section_gallery__img_8} />
-            <img src='/9.png' className={styles.section_gallery__img_9} />
-          </div> */}
-          <h2>This is mobile render.</h2>
-        </>
-      );
-
-    return (
-      <>
-        <h2>This is desktop render</h2>
-        {/* <div className={styles.section_gallery__images}>
-          {renderGalleryContent()}
-          <img src='/1.png' className={styles.section_gallery__img_1} />
-          <img src='/2.png' className={styles.section_gallery__img_2} />
-          <img src='/3.png' className={styles.section_gallery__img_3} />
-          <img src='/41.png' className={styles.section_gallery__img_4} />
-          <img src='/5.png' className={styles.section_gallery__img_5} />
-          <img
-            src='/hands2.png'
-            className={styles.section_gallery__img_hands}
-          />
-          <img src='/72.png' className={styles.section_gallery__img_7} />
-          <img src='/8.png' className={styles.section_gallery__img_8} />
-          <img src='/91.png' className={styles.section_gallery__img_9} />
-        </div> */}
       </>
     );
   };
